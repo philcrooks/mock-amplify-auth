b569a105c05c2c61bb206e4b0475bdeb7bc343d5
Phil Crooks <phil.crooks@bemo.co>
2020-01-29 14:44:01 +0000

    token test updated

--- test/tokens.test.js
+++ test/tokens.test.js
@@ -1,14 +1,16 @@
 const { test } = require("tap");
 const { encodeJwt, decodeJwt } = require("../tokens");
+const { userList } = require("../users");
 
+const [user] = userList;
 let token;
 
 test("can encode a token", async t => {
-  token = await encodeJwt("username", "email@email.com");
+  token = await encodeJwt(user, "username");
 });
 
 test("can decode a token", async t => {
-  const { sub, email } = await decodeJwt(token);
-  t.equal(sub, "username");
-  t.equal(email, "email@email.com");
+  const decoded = await decodeJwt(token);
+  t.equal(decoded.sub, "username");
+  t.equal(decoded.username, user.username);
 });
